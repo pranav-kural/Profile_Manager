33fadf34ad331cd81f424a5c825450eb94c65f53
pranav-kural <pranavkural042@gmail.com>
2022-10-13 15:16:22 -0400

    Added onclick method for second activity

--- app/src/main/java/com/example/profile_manager/Main2Activity.java
+++ app/src/main/java/com/example/profile_manager/Main2Activity.java
@@ -2,7 +2,10 @@ package com.example.profile_manager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+    }
+
+    public void SetTeamIcon(View view) {
+        //Creating a Return intent to pass to the Main Activity
+        Intent returnIntent = new Intent();
+        //Figuring out which image was clicked
+        ImageView selectedImage = (ImageView) view;
+        //Adding stuff to the return intent
+        returnIntent.putExtra("imageID", selectedImage.getId());
+        setResult(RESULT_OK, returnIntent);
+        //Finishing Activity and return to main screen!
+        finish();
     }
 }
